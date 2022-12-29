eeb6c3ad8fb5863f9afedb23eeadd6e237c923b4
angelmarte <robotking134@gmail.com>
2022-12-29 13:51:23 -0500

    Co-authored-by: taylan-P <taylan-P@users.noreply.github.com>

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,25 +1,32 @@
 class ChatsController < ApplicationController
-    # def index
-    #     chats = Chat.all
-    #     render json: chats
-    # end
-    # def create
-    #    chat_present = current_user.chat.all.map
-    #     Participation.create!(participations_params)
-    #     chat = current_user.chat.create!(chat_params)
-    #     render json: chat, status: :created
+    def index
+        chats = Chat.all
+        render json: chats
+    end
+    def create
+       chat_present = current_user.chats.all.include?(participations_params)
+    #    isExist = Conversation.all.map{|c| c.participants}.map{|pArr| pArr.map{|p| p.user_id}.sort}.include?(params[:participants].sort)
+       if chat_present
+        render json: {error:"This chat already exist"},status: :unprocessable_entity
+       else
+        chat = Chat.create!(chat_params)
 
-    # end
+        participations_params.each do |userID|
+                Participation.create!(participations_params)
+       end
+        render json: chat, status: :created
+    end
+end
     # def destroy
     #     chat = Chat.find(params[:id])
     #     chat.destroy
     #     head :no_content
     # end
-    # private
-    # def chat_params
-    #     params.permit(:title, :convo_url)
-    # end
-    # def participations_params
-    #     params.permit(:user_id, :chat_id)
-    # end
+    private
+    def chat_params
+        params.permit(:title, :convo_url, :user_id)
+    end
+    def participations_params
+        params.permit(:user_id, :chat_id)
+    end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,23 +1,23 @@
 class MessagesController < ApplicationController
     def index
-        messi = current_user.messages.all
-        render json: messi
+        message = current_user.messages.all
+        render json: message
     end
-#     def create
-#         message = current_user.messages.create!(message_params)
-#         render json: message, status: :created
-#     end
-#     def destroy
-#         message = current_user.message.find_by(params[:id])
-#         message.destroy
-#         render json: "pop!"
+    def create
+        message = current_user.messages.create!(message_params)
+        render json: message, status: :created
+    end
+    def destroy
+        message = current_user.message.find_by(params[:id])
+        message.destroy
+        render json: "pop!"
 
-#     end
-#     private
-#    def message_params
-#     params.permit(:body,:chat_id)
-#    end
-#    def current_user
-#     User.find_by(id: session[:user_id])
-#    end
+    end
+    private
+   def message_params
+    params.permit(:body,:chat_id)
+   end
+   def current_user
+    User.find_by(id: session[:user_id])
+   end
 end
